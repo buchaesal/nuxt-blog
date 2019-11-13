96da566a032098239908672d80f38de25b4b9eff
DanbiChoi <eks4116@gmail.com>
2019-11-13 16:03:12 +0900

    create pages

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,72 +1,89 @@
-<template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-blog
-      </h1>
-      <h2 class="subtitle">
-        My stunning Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+  .page-home
+    section.intro
+      h1 BLOG 서비스 with Nuxt.js
+    section.featured-posts
+      article.post-preview
+        nuxt-link(:to="'/posts/'+1")
+          figure.post-thumbnail(style="background-image: url(//images.unsplash.com/photo-1571372183175-bbe560b1603a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80)")
+          .post-content
+            h2.post-title 포스트 제목 1
+            p.post-text 포스트 내용
+      article.post-preview
+        nuxt-link(:to="'/posts/'+2")
+          figure.post-thumbnail(style="background-image: url(//images.unsplash.com/photo-1571372183175-bbe560b1603a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80)")
+          .post-content
+            h2.post-title 포스트 제목 2
+            p.post-text 포스트 내용
 </template>
+<style lang="sass" scoped>
+  .intro
+    position: relative
+    height: 200px
+    margin-bottom: 20px
+    padding: 30px
+    background: darken(#5db4f3, 40%) top center
+    background-size: cover
 
-<script>
-import Logo from '~/components/Logo.vue'
+    h1
+      position: absolute
+      top: 5%
+      left: 5%
+      width: 90%
+      font-size: 1.5rem
+      color: lighten(#5db4f3, 20%)
+      background-color: rgba(#5db4f3, 0.26)
+      border-radius: 4px
+      padding: 10px
 
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
+  .featured-posts
+    display: flex
+    justify-content: center
+    align-items: center
+    flex-wrap: wrap
+    padding: 20px
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .post-preview
+    border: 1px solid rgba(#aaa, 0.45)
+    box-shadow: 0 2px 2px rgba(#aaa, 0.45)
+    background-color: #fff
+    flex: 1 0 90%
+    margin:
+      left: 5%
+      right: 5%
+      bottom: 15px
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  a
+    text-decoration: none
+    color: darken(#5db4f3, 20%)
+    transition: all 0.4s ease
+    &:hover .post-content,
+    &:active .post-content
+      background-color: rgba(#5db4f3, 0.26)
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .post-thumbnail
+    width: 100%
+    height: 150px
+    margin: 0
+    background-position: center
+    background-size: cover
 
-.links {
-  padding-top: 15px;
-}
+  .post-title
+    margin-bottom: 0
+
+  .post-text
+    margin-top: 0
+
+  .post-content
+    padding: 10px
+    text-align: center
+
+  @media (min-width: 768px)
+    .intro h1
+      font-size: 2rem
+    .post-preview
+      flex: 0 0 45%
+      margin:
+        left: 10px
+        right: 10px
 </style>
