0c51d98566b25528a3f2407ca9da25a53d5d7c98
DanbiChoi <eks4116@gmail.com>
2019-11-14 17:15:07 +0900

    렌더링 지연 처리 : 싱글 포스트

--- client/pages/posts/_id/index.vue
+++ client/pages/posts/_id/index.vue
@@ -1,19 +1,38 @@
 <template lang="pug">
   .page-single-post
     section.post
-      h1.post-title 포스트 제목
-        .post-info
-          span.post-data 작성일자
-          span.post-author 작성자
-        p.post-content
-          | 포스트 내용
+      h1.post-title {{ loadedPost.title }}
+      .post-info
+        span.post-author {{ loadedPost.author }}
+        span.post-data {{ loadedPost.updatedDate }}
+      img.post-image(:src="loadedPost.thumbnail", alt, width="600")
+      p.post-content {{ loadedPost.content }}
     section.post-feedback
       p
         | 해당 포스트에 대해 궁금한 점이 있으면
         a(href="mailto:yamoo9@naver.com") yamoo9@naver.com
         | 로 문의주세요.
 </template>
-
+<script>
+export default {
+  asyncData({ route }, callback) {
+    setTimeout(() => {
+      callback(null, {
+        // 1초 지연된 데이터 로드 시뮬레이션
+        loadedPost: {
+          id: '1',
+          title: '빅 데이터',
+          author: '야무(yamoo9)',
+          updatedDate: new Date().toLocaleString(),
+          thumbnail: '//goo.gl/mJ5Vsy',
+          content:
+            '빅 데이터란? 기존 데이터베이스 관리도구의 능력을 넘어서는 대량의 정형 또는 비정형 데이터 집합을 포함한 데이터로부터 가치를 추출하고 결과를 분석하는 기술이다.'
+        }
+      })
+    }, 1000)
+  }
+}
+</script>
 <style lang="sass" scoped>
 	.page-single-post
 		padding: 30px
@@ -51,6 +70,20 @@
 		&:active
 			color: darken(#46b784, 20%)
 
+	.post-image
+		margin-top: 20px
+		width: 100%
+		height: auto
+
+	.post-feedback
+		width: 60%
+		margin:
+		top: 30px
+		right: auto
+		left: auto
+		border-top: 1px solid rgba(#000, 0.1)
+		padding-top: 15px
+		
 
 	@media (min-width: 768px)
 		.post
